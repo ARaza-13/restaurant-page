5a83da918eb63027761cf49a21c069371ba23d15
Ali Raza <aliraza021419@gmail.com>
2023-08-02 20:20:09 -0400

    Added production and development configs and installed dev-server

--- webpack.common.js
+++ webpack.common.js
@@ -2,15 +2,14 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: './src/index.js',
-  output: {
-    filename: 'main.[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
+  entry: {
+    app: './src/index.js',
   },
-  plugins: [new HtmlWebpackPlugin({
-    template: "./src/template.html"
-  })],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
   module: {
     rules: [
       {
