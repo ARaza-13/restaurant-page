c70ecd3a6889353d3e772d3f9681d281b281026c
Ali Raza <aliraza021419@gmail.com>
2023-08-08 20:42:40 -0400

    Setup dev server as development tool

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    hot: true,
+  },
   entry: {
     main: './src/index.js',
   },
@@ -16,6 +20,9 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
   module: {
     rules: [
